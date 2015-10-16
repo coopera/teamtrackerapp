1faaa74ab33c8935f540aa4ebd0fc6649caa29dd
Lucas Bibiano <lucasbibianodossantos@gmail.com>
2015-10-15 22:19:41 -0300

    Remove unnecessary code

--- app/services/github_sync_service.rb
+++ app/services/github_sync_service.rb
@@ -6,10 +6,6 @@ class GithubSyncService < ApplicationService
   def perform
     OrgMembersSyncWorker.perform_async(@org)
     OrgRepositoriesSyncWorker.perform_async(@org)
-
-    app_data = AppData.find_by(organization: @org)
-    app_data.gh_last_sync = DateTime.current
-    app_data.save
   end
 
   private
